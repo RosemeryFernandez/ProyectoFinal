2b772495f9ede0ee1723426a01eba1f71fe6e84f
Rosemery Fernandez <rosemeryfernandez97@gmail.com>
2024-11-10 14:24:19 -0500

    Calculo de la mora

---
+++ Solucion/solFile.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from datetime import datetime
+
+#Leer el archivo
+df = pd.read_csv('C:\\Users\\rosem\\Downloads\\Cuadrodepagos_aws.csv')
+
+# Mostrar las primeras filas
+print(df.head())
+# Mostrar los nombres de las columnas
+print(df.columns)
+
+# Convertir las columnas de fechas a formato datetime
+df['Fecha'] = pd.to_datetime(df['Fecha']) # Corresponde a la fecha de vencimiento
+df['Fecha_de_Pago_Aplicado'] = pd.to_datetime(df['Fecha de Pago Aplicado']) #Corresponde a la fecha de pago
+
+# Verificar que las conversiones se realizaron correctamente
+print(df[['Fecha', 'Fecha_de_Pago_Aplicado']].head())
+
+
+#1) Cálculo de días mora con la fecha corte
+
+#fecha_str = input("Ingresa una fecha (YYYY-MM-DD): ")
+fecha_actual = datetime.today()
+
+# Función para calcular los días de mora
+def calcular_dias_mora(Fecha, Fecha_de_Pago_Aplicado):
+    if pd.isna(Fecha_de_Pago_Aplicado) or Fecha_de_Pago_Aplicado > fecha_actual:  # No ha pagado o está por pagar
+        dias_mora = (fecha_actual - Fecha).days
+        return max(dias_mora, 0)  # No debe haber valores negativos
+    else:
+        return 0  # No hay mora si ya pagó
+
+df['dias_mora'] = df.apply(lambda row: calcular_dias_mora(row['Fecha'], row['Fecha_de_Pago_Aplicado']), axis=1)
+
+print(df[['ID Contrato', 'Fecha', 'Fecha_de_Pago_Aplicado', 'dias_mora']].head())
+
+
+
